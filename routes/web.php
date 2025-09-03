886c0eea8befa3d61b9cbcd07052e2688d592dca
Keyberth <devkeyberth@gmail.com>
2025-09-03 15:49:26 -0400

    Ruta dinamica detalles

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,14 @@
 <?php
 
 use App\Http\Controllers\PageController;
+use App\Http\Controllers\QuestionController;
 use App\Livewire\Settings\Appearance;
 use App\Livewire\Settings\Password;
 use App\Livewire\Settings\Profile;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PageController::class, 'index'])->name('home');
+Route::get('question/{question}', [QuestionController::class, 'show'])->name('question.show');
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
