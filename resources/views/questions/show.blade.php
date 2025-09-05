34072e9dbd957576453f78685e64aef7946dd7d0
Keyberth <devkeyberth@gmail.com>
2025-09-05 16:20:56 -0400

    Componente Livewire para comentarios

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -37,34 +37,7 @@
         </p>
 
         <!-- Comments -->
-        <ul class="my-4 space-y-2">
-
-    @foreach ($question->comments as $comment)
-
-
-    <li class="flex items-center gap-2">
-        <p class="text-xs bg-white/10 p-4 rounded-md">
-            <span class="text-gray-500">
-                {{ $comment->user->name }} |
-                {{ $comment->created_at->diffForHumans() }}
-            </span>
-            <span class="text-gray-300">
-                {{ $comment->content }}
-            </span>
-        </p>
-
-        <div>&hearts;</div>
-    </li>
-
-    @endforeach
-
-</ul>
-
-<p class="text-gray-500">
-    <a href="#" class="rounded-md text-xs hover:underline cursor-pointer">
-        Agregar comentario
-    </a>
-</p>
+       <livewire:comment :commentable="$question" />
     </div>
 
     <ul class="space-y-4">
@@ -85,34 +58,7 @@
                     </p>
 
                     <!-- Comments -->
-                          <ul class="my-4 space-y-2">
-
-    @foreach ($answer->comments as $comment)
-
-
-    <li class="flex items-center gap-2">
-        <p class="text-xs bg-white/10 p-4 rounded-md">
-            <span class="text-gray-500">
-                {{ $comment->user->name }} |
-                {{ $comment->created_at->diffForHumans() }}
-            </span>
-            <span class="text-gray-300">
-                {{ $comment->content }}
-            </span>
-        </p>
-
-        <div>&hearts;</div>
-    </li>
-
-    @endforeach
-
-</ul>
-
-<p class="text-gray-500">
-    <a href="#" class="rounded-md text-xs hover:underline cursor-pointer">
-        Agregar comentario
-    </a>
-</p>
+                    <livewire:comment :commentable="$answer" />
                 </div>
             </div>
         </li>
